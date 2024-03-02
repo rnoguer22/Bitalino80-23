55891d082cae0603fac2f91230dc60591edb4d48
rnoguer22 <rnogugon@myuax.com>
2024-03-02 14:17:57 +0100

    Organizamos los datos para su analisis

--- txt-csv.py
+++ txt-csv.py
@@ -14,34 +14,39 @@ class To_csv():
 
     def to_csv(self):
         for file in self.txt_names:
+            #Obtenemos el tipo de archivo, para guardarlo posteriormente en la carpeta correspondiente
+            tipo = file[:3]
+            if tipo == 'ACC':
+                tipo = 'ACCyLUX'
+            elif tipo == 'ECG':
+                if file[-5] == '2':
+                    tipo = 'ECG2'
+            #Creamos las carpetas si no existen
+            if not os.path.exists('csv/' + tipo):
+                os.makedirs('csv/' + tipo)
             with open('Data/' + file, 'r') as in_file:
                 stripped = (line.strip() for line in in_file)
                 lines = (line.split("\t") for line in stripped if line)
                 lines_after_4 = islice(lines, 3, None)  # Empieza a partir de la línea 4
-                with open('csv/' + file + '.csv', 'w', newline='') as out_file:
+                with open('csv/' + tipo + '/' + file[:-4] + '.csv', 'w', newline='') as out_file:
                     writer = csv.writer(out_file)
-                    columnas = self.get_data(file)
+                    columnas = self.get_columns(file)
                     writer.writerow(columnas)
                     for line in lines_after_4:
                         writer.writerow(line)
 
     
-    def get_data(self, fichero):
+    #Metodo para obtener las columnas de los datos
+    def get_columns(self, fichero):
         with open(f'Data/{fichero}', 'r', encoding='utf-8') as file:
             data = file.readlines()
             linea = data[1]
             indice_column = linea.find('"column":')
-            # Si se encuentra la cadena 'column'
             if indice_column != -1:
-                # Extraer la subcadena que comienza después de 'column":'
                 subcadena = linea[indice_column + len('"column":'):]
-                # Encontrar el índice del primer corchete cuadrado '[' después de 'column":'
                 indice_corchete_inicio = subcadena.find('[')
-                # Encontrar el índice del último corchete cuadrado ']' después de '['
                 indice_corchete_fin = subcadena.find(']', indice_corchete_inicio)
-                # Extraer la lista como una subcadena
                 lista_subcadena = subcadena[indice_corchete_inicio + 1:indice_corchete_fin]
-                # Convertir la subcadena a una lista de elementos
                 lista = lista_subcadena.split(',')
                 lista = [elem.strip().strip('"') for elem in lista]
                 return lista
