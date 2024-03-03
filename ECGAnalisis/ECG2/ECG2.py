374a1ab2f7145a706371577f0af54f6883242dce
rnoguer22 <rnogugon@myuax.com>
2024-03-03 19:52:07 +0100

    Metodo para guardar la grafica de los datos

--- ECGAnalisis/ECG2/ECG2.py
+++ ECGAnalisis/ECG2/ECG2.py
@@ -8,13 +8,17 @@ import matplotlib.pyplot as plt
 
 class ECG2:
 
-    def __init__(self, file):
-        self.file = file
-        self.df = pd.read_csv(file)
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.df = pd.read_csv(data_path)
     
 
     def get_columns(self):
         return self.df.columns
+    
+
+    def get_name(self):
+        return self.data_path[15:-6]
     
 
     #Metodo para aplicar un filtro de media móvil y suavizar la señal y eliminar el ruido
@@ -28,9 +32,13 @@ class ECG2:
         return df_normalized   
 
 
-    def find_peaks(self):
-        ecg_data = self.data_cleaning()['A2'].values
+    def plot_peaks(self):
+        ecg_data = self.df['A2'].values
 
+        num_muestras = self.df.shape[0]
+        tiempo = 21
+        frecuencia_muestreo = num_muestras / tiempo  
+        print(frecuencia_muestreo)  
 
         # Encuentra los picos R en la señal de ECG
         peaks, _ = find_peaks(ecg_data, height=0)  # Ajusta el valor de altura según tu señal
@@ -39,10 +47,10 @@ class ECG2:
         plt.figure(figsize=(12, 6))
         plt.plot(ecg_data, color='blue')
         plt.plot(peaks, ecg_data[peaks], "x", color='red', markersize=10)
-        plt.title('Señal de ECG con Picos R Identificados')
-        plt.xlabel('Tiempo (muestras)')
+        plt.title('ECG {} con Picos R Identificados'.format(self.get_name()))
+        plt.xlabel('Muestras')
         plt.ylabel('Amplitud')
-        plt.show()
+        plt.savefig('./ECGAnalisis/ECG2/img/{}_ECG_peaks.png'.format(self.get_name()))
 
         # Calcula la frecuencia cardíaca utilizando los picos R identificados
         tiempo_entre_picos = np.diff(peaks)
@@ -50,6 +58,6 @@ class ECG2:
         print("Frecuencia Cardíaca:", frecuencia_cardiaca, "latidos por minuto")
 
 
-ecg2 = ECG2('./csv/ECG2/ECG_Esther_2.csv')
+ecg2 = ECG2('./csv/ECG2/ECG_Moyis_2.csv')
 print(ecg2.get_columns())
 ecg2.find_peaks()
