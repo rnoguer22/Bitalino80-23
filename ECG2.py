87e57faba303332c5a4a0879d548a106ca242634
rnoguer22 <rnogugon@myuax.com>
2024-03-03 18:41:38 +0100

    Metodo data_cleaning, para suavizar la señal y eliminar el ruido

--- ECG2.py
+++ ECG2.py
@@ -13,6 +13,17 @@ class ECG2:
         return self.df.columns
     
 
+    #Metodo para aplicar un filtro de media móvil y suavizar la señal y eliminar el ruido
+    def data_cleaning(self): 
+        window_size = 3
+        df_smoothed = self.df.rolling(window=window_size).mean().dropna()
+        #Normalizamos los datos para que estén en un rango de 0 a 1
+        df_normalized = (df_smoothed - df_smoothed.min()) / (df_smoothed.max() - df_smoothed.min())
+        #Eliminamos las señales que de las que no hemos recogido datos
+        df_normalized = df_normalized.drop(['I1', 'I2', 'O1', 'O2'], axis=1)
+        return df_normalized    
+
 
 ecg2 = ECG2('./csv/ECG2/ECG_Moyis_2.csv')
 print(ecg2.get_columns())
+print(ecg2.data_cleaning())
