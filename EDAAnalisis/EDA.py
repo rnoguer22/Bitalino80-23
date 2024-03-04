9a8a4336f46ae2de1e83dd4c0004156eead8a814
tereesaalvarez <alvdeporteresa@gmail.com>
2024-03-04 11:28:41 +0100

    extraccion picos de las señales

--- EDAAnalisis/EDA.py
+++ EDAAnalisis/EDA.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from scipy.signal import medfilt
+from scipy.signal import find_peaks
 import numpy as np
 
 #cargar csv
@@ -25,3 +26,22 @@ eda_teresa['A3_preprocesado'] = preprocesar_eda(eda_teresa)
 
 #visualizar
 print(eda_esther.head(), eda_moyis.head(), eda_pelayo.head(), eda_teresa.head())
+
+#Funcion para extraccion de características de los picos de la señal EDA
+def extraer_caracteristicas_eda(data):
+    #picos
+    picos, _ = find_peaks(data, height=0.1)#para probar acuerdate de cambiarlo si esta mal
+    #caracteristicas de los picos
+    numero_picos = len(picos)
+    altura_picos = data[picos].mean() if numero_picos > 0 else 0
+
+    return numero_picos, altura_picos
+
+#aplicar extraccion de caracteristicas a cada conjunto de datos
+caracteristicas_esther = extraer_caracteristicas_eda(eda_esther['A3_preprocesado'])
+caracteristicas_moyis = extraer_caracteristicas_eda(eda_moyis['A3_preprocesado'])
+caracteristicas_pelayo = extraer_caracteristicas_eda(eda_pelayo['A3_preprocesado'])
+caracteristicas_teresa = extraer_caracteristicas_eda(eda_teresa['A3_preprocesado'])
+
+#visualizar
+print(caracteristicas_esther, caracteristicas_moyis, caracteristicas_pelayo, caracteristicas_teresa)
