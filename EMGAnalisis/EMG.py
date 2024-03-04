aef8ef9df6ef9d19b9ad06d2e43be1052e4a616f
Pelayo <phuermij@myuax.com>
2024-03-04 16:15:16 +0100

    cambios

--- EMGAnalisis/EMG.py
+++ EMGAnalisis/EMG.py
@@ -10,8 +10,8 @@ class EMG():
         self.data = pd.read_csv(ruta_csv, header = 0)
         self.nombre = nombre
 
-    def get_cols(self):
-        return self.data.columns
+    def raw(self):
+        self.signals.plot()
     
     def clean(self):
         self.data.drop(columns=['I1', 'I2', 'O1', 'O2'], inplace=True)
@@ -30,31 +30,39 @@ class EMG():
         data_cleaned = data[~outliers.any(axis=1)]
         return data_cleaned
     
-    def noise(self, data):
-        fc = 1000
-        fc_norm = fc / (10000 / 2)
+    def preprocessing(self):
+        
+        data = self.data
 
-        b, a = butter(4, fc_norm, btype='low')
+        #Normalizamos los datos con respecto a la media
+        data['A1'] = np.abs(data['A1'] - data['A1'].mean())
+
+        #Eliminamos cualquier posible ruido que pueda haber
+        fc = 1000
+        fc_norm = fc / (5000 / 2)
+
+        b, a = butter(2, fc_norm, btype='low')
         data['A1'] = lfilter(b, a, data['A1'])
+
+        #Normalizamos los datos con respecto al maximo
+        emg_max = data['A1'].max()
+
+        data = data / emg_max * 100
+
         return data
     
     def group(self, data):
         data_grouped = data.groupby('nSeq').mean()
         return data_grouped
-    
-    def graph(self):
-        self.signals.plot()
-        plt.savefig('EMGAnalisis/img/EMG_{}.png'.format(self.nombre))
-        
 
     def plot(self, data):
         data_reset = data.reset_index()
-        plt.plot(data_reset['nSeq'], data_reset['A1'], label='EMG_grouped')
-        plt.xlabel('nSeq')
+        plt.plot(data_reset['A1'], label='EMG')
+        plt.xlabel('Tiempo')
         plt.ylabel('Amplitud')
-        plt.title('Señales de EMG Limpias')
+        plt.title('Señales de EMG de {}'.format(self.nombre))
         plt.legend()
-        plt.savefig('EMGAnalisis/img/EMG_{}_media.png'.format(self.nombre))
+        #plt.savefig('EMGAnalisis/img/EMG_{}_media.png'.format(self.nombre))
         plt.show()
 
 
@@ -62,13 +70,16 @@ class EMG():
 if __name__ == "__main__":
     emg = EMG("Pelayo", "./Data/EMG_Pelayo.txt", "./csv/EMG/EMG_Pelayo.csv")
 
+    
+
     #Limpieza de datos
-    data_cleaned = emg.clean()
-    outliers = emg.outliers(data_cleaned)
-    group = emg.group(outliers)
+    #data_cleaned = emg.clean()
+    #outliers = emg.outliers(data_cleaned)
+    #emg.preprocessing(outliers)
+    
 
     #Grafico inicial
-    emg.graph()
+    emg.preprocessing()
 
     #Grafico agrupando las frecuencias y tomamdo la media
     #emg.plot(group)
