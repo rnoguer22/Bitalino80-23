128d470ee57e0feccb8bdaf497be7790a465a6cf
rnoguer22 <rnogugon@myuax.com>
2024-03-04 13:27:03 +0100

    Solucionamos los errores en el pulso

--- ECGAnalisis/ECG2/ECG2.py
+++ ECGAnalisis/ECG2/ECG2.py
@@ -21,51 +21,65 @@ class ECG2:
         return self.data_path[15:-6]
     
 
+    def get_num_segments(self, cleaned_signal, seconds=10, sampling_rate=100):
+        window_size = sampling_rate * seconds
+        num_samples = len(cleaned_signal)
+        num_segments = num_samples // window_size
+        return num_segments
+    
+
     #Metodo para aplicar un filtro de media móvil, suavizar la señal y eliminar el ruido
-    def data_cleaning(self): 
-        window_size = 3
-        df_smoothed = self.df.rolling(window=window_size).mean().dropna()
-        #Normalizamos los datos para que estén en un rango de 0 a 1
-        df_normalized = (df_smoothed - df_smoothed.min()) / (df_smoothed.max() - df_smoothed.min())
-        #Eliminamos las señales que de las que no hemos recogido datos
-        df_normalized = df_normalized.drop(['I1', 'I2', 'O1', 'O2'], axis=1)
-        return df_normalized   
+    def data_cleaning(self, window_size=3): 
+        cleaned_signal = self.df.rolling(window=window_size).mean().dropna()
+        return cleaned_signal   
 
 
     #Metodo para graficar la señal de ECG y los picos R identificados
-    def plot_peaks(self):
-        ecg_data = self.df['A2'].values 
-        # Encuentramos los picos R en la señal de ECG
-        self.peaks, _ = find_peaks(ecg_data, height=0)  # Ajusta el valor de altura según tu señal
-        #Guardamos la grafica del ECG y los picos
-        plt.figure(figsize=(12, 6))
-        plt.plot(ecg_data, color='blue')
-        plt.plot(self.peaks, ecg_data[self.peaks], "x", color='red', markersize=10)
-        plt.title('ECG {} con Picos R Identificados'.format(self.get_name()))
-        plt.xlabel('Muestras')
-        plt.ylabel('Amplitud')
-        plt.savefig('./ECGAnalisis/ECG2/img/{}_ECG_peaks.png'.format(self.get_name()))
+    def plot_peaks(self, sampling_rate=100):
+        cleaned_signal = self.data_cleaning()['A2'].values
+
+        num_segments = self.get_num_segments(cleaned_signal)
+
+        if num_segments > 0:
+            segments = np.array_split(cleaned_signal, num_segments)
+            heart_rates = []
+
+            for i, segment in enumerate(segments):
+                peaks, _ = find_peaks(segment, distance=sampling_rate/2)
+                heart_rate = self.get_cardio_freq(peaks, sampling_rate)
+                heart_rates.append(heart_rate)
+                #Guardamos la grafica del ECG y los picos
+                plt.figure(figsize=(12, 6))
+                plt.plot(segment, color='blue')
+                plt.plot(peaks, segment[peaks], "x", color='red', markersize=10)
+
+            plt.title('ECG {} con Picos R Identificados'.format(self.get_name()))
+            plt.xlabel('Muestras')
+            plt.ylabel('Amplitud')
+            plt.savefig('./ECGAnalisis/ECG2/img/{}_ECG_peaks.png'.format(self.get_name()))
+            avg_heart_rate = np.mean(heart_rates)
+            print('Frecuencia cardiaca media {}: {} latidos por minuto'.format(self.get_name(), avg_heart_rate))
+        else:
+            print(f'No hay suficientes datos de {self.get_name()} para formar 10 s.')
     
 
     #Metodo para obtener la frecuencia cardíaca
-    def get_cardio_freq(self, tiempo):
-        num_muestras = self.df.shape[0]
-        frecuencia_muestreo = num_muestras / tiempo  
-        # Calcula la frecuencia cardíaca utilizando los picos de la señal
-        tiempo_entre_picos = np.diff(self.peaks)
-        frecuencia_cardiaca = 60 / (np.mean(tiempo_entre_picos) / frecuencia_muestreo)
+    def get_cardio_freq(self, peaks, sampling_rate=100):
+        # Calculamos la frecuencia cardíaca utilizando los picos de la señal
+        rr_intervals = np.diff(peaks) / sampling_rate
+        frecuencia_cardiaca = 60 / np.mean(rr_intervals)
         return round(frecuencia_cardiaca)
 
     
     #Metodo para obtener los resultados de cada usuario
     def get_results(self):
         directory = './csv/ECG2/'
-        files = os.listdir('./csv/ECG2/')
+        files = os.listdir(directory)
         for file in files:
             dir_ = directory + file
             ecg2 = ECG2(dir_)
-            ecg2.plot_peaks()
-            print("Frecuencia cardiaca {}: {} latidos por minuto".format(ecg2.get_name(), ecg2.get_cardio_freq(360)))
+            ecg2.plot_peaks()    
+
 
 
 if __name__ == '__main__':
