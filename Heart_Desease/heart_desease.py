166c7c23fb5b5b20ede397c16d835bf9bc9606be
rnoguer22 <rnogugon@myuax.com>
2024-05-01 19:17:50 +0200

    Visualizamos los datos numericos del df

--- Heart_Desease/heart_desease.py
+++ Heart_Desease/heart_desease.py
@@ -2,6 +2,7 @@ import warnings
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
 
@@ -11,6 +12,8 @@ class Heart_Desease():
     def __init__(self, data_path):
         self.data_path = data_path
         self.data = pd.read_csv(data_path)
+        #Definimos una paleta de colores para usar en los graficos
+        self.mypal = ['#FC05FB', '#FEAEFE', '#FCD2FC','#F3FEFA', '#B4FFE4','#3FFEBA']
         warnings.filterwarnings('ignore')
 
     def get_data(self):
@@ -38,6 +41,7 @@ class Heart_Desease():
                     'ca':'num_major_vessels', 
                     'thal': 'thalassemia'})
     
+
 
     #Metodo para renombrar los datos y que quede mas clara la informacion
     def rename_data(self):
@@ -68,14 +72,13 @@ class Heart_Desease():
         self.data['thalassemia'][self.data['thalassemia'] == 3] = 'reversable defect'
     
 
+
     #Metodo para ver las personas con enfermedades cardiovasculares
     def show_target_distribution(self, data):
-        #Definimos una paleta de colores para usar en el grafico
-        mypal= ['#FC05FB', '#FEAEFE', '#FCD2FC','#F3FEFA', '#B4FFE4','#3FFEBA']
         plt.figure(figsize=(7, 5),facecolor='#F6F5F4')
         total = float(len(data))
         #De la columna target, 0 son los que no tienen riesgo de enfermedad cardiovascular y 1 los que si
-        ax = sns.countplot(x=data['target'], palette=mypal[1::4])
+        ax = sns.countplot(x=data['target'], palette=self.mypal[1::4])
         ax.set_facecolor('#F6F5F4')
 
         for p in ax.patches:
@@ -89,10 +92,45 @@ class Heart_Desease():
         plt.savefig('./img/target_distribution.png')
 
 
+    
+    #Metodo para ver las graficas de los datos numericos del dataframe
+    def numeric_density_plots(self, data):
+        numeric_cols = data.select_dtypes(include=['number'], exclude=['bool']).columns.tolist()
+        L = len(numeric_cols)
+        ncol= 2
+        nrow= int(np.ceil(L/ncol))
+        #remove_last= (nrow * ncol) - L
+
+        fig, ax = plt.subplots(nrow, ncol, figsize=(16, 14),facecolor='#F6F5F4')   
+        fig.subplots_adjust(top=0.92)
+
+        i = 1
+        for col in numeric_cols:
+            plt.subplot(nrow, ncol, i, facecolor='#F6F5F4')
+            ax = sns.kdeplot(data=data, x=col, hue="target", multiple="stack", palette=self.mypal[1::4]) 
+            ax.set_xlabel(col, fontsize=20)
+            ax.set_ylabel("density", fontsize=20)
+            sns.despine(right=True)
+            sns.despine(offset=0, trim=False)
+            if col == 'num_major_vessels':
+                sns.countplot(data=data, x=col, hue="target", palette=self.mypal[1::4])
+                for p in ax.patches:
+                        height = p.get_height()
+                        ax.text(p.get_x()+p.get_width()/2.,height + 3,'{:1.0f}'.format((height)),ha="center",
+                            bbox=dict(facecolor='none', edgecolor='black', boxstyle='round', linewidth=0.5))
+            i = i +1
+
+        plt.suptitle('Distribution of Numerical Features' ,fontsize = 24)
+        plt.show()
+
+
 
 heart_des = Heart_Desease('./csv/heart.csv')
 heart_des.drop_data()
 heart_des.rename_columns()
 heart_des.rename_data()
 data = heart_des.get_data()
-heart_des.show_target_distribution(data)
+#heart_des.show_target_distribution(data)
+
+print(data.describe().T)
+heart_des.numeric_density_plots(data)
