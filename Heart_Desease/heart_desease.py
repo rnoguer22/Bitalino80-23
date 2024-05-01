f8e83019bfdc8d4fecd9b6b2f99b4f108b94878a
rnoguer22 <rnogugon@myuax.com>
2024-05-01 15:17:34 +0200

    Actualizamos el nombre de las columnas

--- Heart_Desease/heart_desease.py
+++ Heart_Desease/heart_desease.py
@@ -4,19 +4,29 @@ class Heart_Desease():
 
     def __init__(self, data_path):
         self.data_path = data_path
-    
-    def get_df(self):
-        df = pd.read_csv(self.data_path)
-        return df
+        self.data = pd.read_csv(data_path)
     
     #Metodo para eliminar datos incorrectos (ca y thal)
-    def drop_data(self, df):
-        df = df[df['ca'] < 4]
-        df = df[df['thal'] > 0]
-        return df
+    def drop_data(self):
+        self.data = self.data[self.data['ca'] < 4]
+        self.data = self.data[self.data['thal'] > 0]
+    
+    def rename_columns(self):
+        self.data = self.data.rename(
+            columns = {'cp':'chest_pain_type', 
+                    'trestbps':'resting_blood_pressure', 
+                    'chol': 'cholesterol',
+                    'fbs': 'fasting_blood_sugar',
+                    'restecg' : 'resting_electrocardiogram', 
+                    'thalach': 'max_heart_rate_achieved', 
+                    'exang': 'exercise_induced_angina',
+                    'oldpeak': 'st_depression', 
+                    'slope': 'st_slope', 
+                    'ca':'num_major_vessels', 
+                    'thal': 'thalassemia'})
     
 
 heart_des = Heart_Desease('./Heart_Desease/heart.csv')
-initial_df = heart_des.get_df()
-clean_df = heart_des.drop_data(initial_df)
-print(len(clean_df))
+heart_des.drop_data()
+heart_des.rename_columns()
+print(heart_des.data.head())
