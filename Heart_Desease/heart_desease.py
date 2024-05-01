324630e22676f74dd03779671a11bad5501abff6
rnoguer22 <rnogugon@myuax.com>
2024-05-01 15:13:34 +0200

    Eliminamos datos incorrectos

--- Heart_Desease/heart_desease.py
+++ Heart_Desease/heart_desease.py
@@ -4,8 +4,19 @@ class Heart_Desease():
 
     def __init__(self, data_path):
         self.data_path = data_path
-        self.df = pd.read_csv(data_path)
+    
+    def get_df(self):
+        df = pd.read_csv(self.data_path)
+        return df
+    
+    #Metodo para eliminar datos incorrectos (ca y thal)
+    def drop_data(self, df):
+        df = df[df['ca'] < 4]
+        df = df[df['thal'] > 0]
+        return df
     
 
 heart_des = Heart_Desease('./Heart_Desease/heart.csv')
-print(heart_des.df.head())
+initial_df = heart_des.get_df()
+clean_df = heart_des.drop_data(initial_df)
+print(len(clean_df))
