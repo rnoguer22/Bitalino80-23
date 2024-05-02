d0bebfe049f4d6ea4cc5bea82f8d67f3db47629d
rnoguer22 <rnogugon@myuax.com>
2024-05-02 22:41:39 +0200

    Metodo para obtener el train y  test de los datos

--- Heart_Desease/heart_pred.py
+++ Heart_Desease/heart_pred.py
@@ -21,46 +21,67 @@ from heart_analysis import Heart_Analysis
 
 
 
-
 class Heart_Pred(Heart_Analysis):
 
     def __init__(self, data_path):
         self.data_path = data_path
-        heart_des = Heart_Analysis(data_path)
-        heart_des.drop_data()
-        heart_des.rename_columns()
-        heart_des.rename_data()
-        self.data = heart_des.get_data()
-        seed = 0
-        self.names = [
-            'Logistic Regression',
-            'Nearest Neighbors',
-            'Support Vectors',
-            'Nu SVC',
-            'Decision Tree',
-            'Random Forest',
-            'AdaBoost',
-            'Gradient Boosting',
-            'Naive Bayes',
-            'Linear DA',
-            'Quadratic DA',
-            "Neural Net"
-        ]
-        self.classifiers = [
-            LogisticRegression(solver="liblinear", random_state=seed),
+        self.seed = 0
+    
+
+    def get_pred_names(self):
+        names = ['Logistic Regression',
+                'Nearest Neighbors',
+                'Support Vectors',
+                'Nu SVC',
+                'Decision Tree',
+                'Random Forest',
+                'AdaBoost',
+                'Gradient Boosting',
+                'Naive Bayes',
+                'Linear DA',
+                'Quadratic DA',
+                'Neural Net']
+        return names
+
+
+    def get_pred_classifiers(self):
+        classifiers = [
+            LogisticRegression(solver="liblinear", random_state=self.seed),
             KNeighborsClassifier(2),
-            SVC(probability=True, random_state=seed),
-            NuSVC(probability=True, random_state=seed),
-            DecisionTreeClassifier(random_state=seed),
-            RandomForestClassifier(random_state=seed),
-            AdaBoostClassifier(random_state=seed),
-            GradientBoostingClassifier(random_state=seed),
+            SVC(probability=True, random_state=self.seed),
+            NuSVC(probability=True, random_state=self.seed),
+            DecisionTreeClassifier(random_state=self.seed),
+            RandomForestClassifier(random_state=self.seed),
+            AdaBoostClassifier(random_state=self.seed),
+            GradientBoostingClassifier(random_state=self.seed),
             GaussianNB(),
             LinearDiscriminantAnalysis(),
             QuadraticDiscriminantAnalysis(),
-            MLPClassifier(random_state=seed),
+            MLPClassifier(random_state=self.seed),
         ]
+        return classifiers
+
+
+    def get_clean_data(self):
+        heart_des = Heart_Analysis(self.data_path)
+        heart_des.drop_data()
+        heart_des.rename_columns()
+        heart_des.rename_data()
+        return heart_des.get_data()
+    
+
+    #Metodo para obtener el train y test de los datos
+    def train_test_split(self, data):
+        test_size = 0.25
+        features = data.columns[:-1]
+        X = data[features]
+        y = data['target']
+        return train_test_split(X, y, test_size = test_size, random_state=self.self.seed)
+    
+
 
 
 if __name__ == '__main__':
     heart_pred = Heart_Pred('./csv/heart.csv')
+    data = heart_pred.get_clean_data()
+    X_train, X_val, y_train, y_val = heart_pred.train_test_split()
