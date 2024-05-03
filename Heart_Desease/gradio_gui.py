33814c5a5a970b2a10a3379644becdd12a4cdf0f
rnoguer22 <rnogugon@myuax.com>
2024-05-03 18:27:29 +0200

    Cambios en la interfaz

--- Heart_Desease/gradio_gui.py
+++ Heart_Desease/gradio_gui.py
@@ -5,25 +5,33 @@ import gradio as gr
 
 class Gradio_GUI():
         
+    #Metodo para devolver el path de la imagen a mostrar
     def select_analysis(self, analysis_type):
-        base_path = './img/analysis/'
-        if analysis_type == 'Target Distribution':
-            analysis = 'target_distribution.png'
-        elif analysis_type == 'Distribution of Categorical Features':
-            analysis = 'cat_count_plots.png'
-        elif analysis_type == 'Distribution of Numerical Features':
-            analysis = 'num_density_plots.png'
-        elif analysis_type == 'Pairplot of Numerical Features':
-            analysis = 'num_pairplot.png'
-        elif analysis_type == 'Pearson Heatmap':
-            analysis = 'pearson_corr.png'
-        elif analysis_type == 'Cramer V Heatmap':
-            analysis = 'cramers_corr.png'
-        elif analysis_type == 'Reg Plots':
-            analysis = 'reg_plots.png'
+        base_path = './img/predictions/'
+        if analysis_type == 'Confusion Matrixes':
+            analysis = 'confusion_matrixes.png'
+        elif analysis_type == 'ROC Curves':
+            analysis = 'roc_auc_curve.png'
+        else:
+            base_path = './img/analysis/'
+            if analysis_type == 'Target Distribution':
+                analysis = 'target_distribution.png'
+            elif analysis_type == 'Distribution of Categorical Features':
+                analysis = 'cat_count_plots.png'
+            elif analysis_type == 'Distribution of Numerical Features':
+                analysis = 'num_density_plots.png'
+            elif analysis_type == 'Pairplot of Numerical Features':
+                analysis = 'num_pairplot.png'
+            elif analysis_type == 'Pearson Heatmap':
+                analysis = 'pearson_corr.png'
+            elif analysis_type == 'Cramer V Heatmap':
+                analysis = 'cramers_corr.png'
+            elif analysis_type == 'Reg Plots':
+                analysis = 'reg_plots.png'
         return os.path.join(base_path, analysis)
 
-    
+
+    #Metodo para lanzar la interfaz de gradio    
     def launch_gradio_gui(self):
         with gr.Blocks(theme=gr.themes.Monochrome()) as demo:
             gr.Markdown('''
@@ -34,13 +42,10 @@ class Gradio_GUI():
             with gr.Tabs():
 
                 with gr.TabItem('Analysis'):
-                    with gr.Row():
-                        with gr.Column():
-                            analysis_type = ['Target Distribution', 'Distribution of Categorical Features', 'Distribution of Numerical Features', 'Pairplot of Numerical Features', 'Pearson Heatmap', 'Cramer V Heatmap', 'Reg Plots']
-                            dropdown_analyis_type = gr.Dropdown(choices=analysis_type, value=analysis_type[0], label="Choose the cluster to launch:")
-                            gr.Markdown('<br><br><br><br><br><br>') 
-                            text_button = gr.Button("Generate")
-                        analysis_img = gr.Image(height=445)
+                    analysis_type = ['Target Distribution', 'Distribution of Categorical Features', 'Distribution of Numerical Features', 'Pairplot of Numerical Features', 'Pearson Heatmap', 'Cramer V Heatmap', 'Reg Plots', 'Confusion Matrixes', 'ROC Curves']
+                    dropdown_analyis_type = gr.Dropdown(choices=analysis_type, value=analysis_type[-3], label="Choose the cluster to launch:")
+                    text_button = gr.Button("Generate") 
+                    analysis_img = gr.Image()
                     text_button.click(self.select_analysis, inputs=dropdown_analyis_type, outputs=analysis_img)
 
                 '''with gr.TabItem('Predictions'):
